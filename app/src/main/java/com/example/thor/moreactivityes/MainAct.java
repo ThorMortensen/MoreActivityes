06b9251670720713dea7f262d81546f86e77423d
ThorMortensen <Send.tilmig@gmail.com>
2014-08-20 23:57:22 +0200

    Første implicit

--- app/src/main/java/com/example/thor/moreactivityes/MainAct.java
+++ app/src/main/java/com/example/thor/moreactivityes/MainAct.java
@@ -1,12 +1,20 @@
 package com.example.thor.moreactivityes;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 public class MainAct extends Activity {
+
+    private static final String LOGTAG = "MainAct";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,4 +41,78 @@ public class MainAct extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void secondAct(View view)
+    {
+        Intent intent = new Intent(this,SecondActiv.class);
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(LOGTAG, "On Destroy");
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(LOGTAG, "On Start");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d(LOGTAG, "On Stop");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d(LOGTAG, "On Restart");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        Log.d(LOGTAG, "On Resume");
+        super.onResume();
+    }
+
+    public void click(View view)
+    {
+        Intent implIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.jetbrains.com/idea/webhelp/viewing-method-parameter-information.html"));
+        startActivity(implIntent);
+
+        Button but3 = new Button(this);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.mainLay);
+
+        but3.setText("Try me");
+        layout.addView(but3);
+
+        but3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                nextClick();
+            }
+        });
+    }
+
+    private void nextClick()
+    {
+        Intent nextImpInt = new Intent(Intent.ACTION_SEND);
+        nextImpInt.putExtra(Intent.EXTRA_TEXT,"blabvlaba");
+        nextImpInt.setType("text/plain");
+        startActivity(nextImpInt);
+    }
+
+
 }
--- app/src/main/java/com/example/thor/moreactivityes/SecondActiv.java
+++ app/src/main/java/com/example/thor/moreactivityes/SecondActiv.java
@@ -2,16 +2,47 @@ package com.example.thor.moreactivityes;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.AndroidCharacter;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 
 public class SecondActiv extends Activity {
+
+    private static final String LOGTAG = "SecondAct";
+    LinearLayout view;
+    int butNr = 0;
+
+
+    private void makeBut ()
+    {
+        Button button = new Button(this);
+        button.setText("But "+ butNr++ );
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                makeBut();
+            }
+        });
+
+
+        view.addView(button);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        view = (LinearLayout) findViewById(R.id.topLin);
+        makeBut();
     }
 
 
@@ -31,6 +62,43 @@ public class SecondActiv extends Activity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if (id == android.R.id.home)
+            finish();
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(LOGTAG, "On Destroy");
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(LOGTAG, "On Start");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d(LOGTAG, "On Stop");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d(LOGTAG, "On Restart");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        Log.d(LOGTAG, "On Resume");
+        super.onResume();
     }
 }
